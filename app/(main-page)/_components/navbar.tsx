f85ff69e8704ea23a10fb6f3e4ac7ec00e3e7964
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-04-22 11:40:34 +0200

    Fix navbar on wider screens than 1920 pixels (#52)
    Co-authored-by: Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>

--- app/(main-page)/_components/navbar.tsx
+++ app/(main-page)/_components/navbar.tsx
@@ -11,66 +11,68 @@ import { source_sans_3 } from "@/app/fonts";
 export default function Navbar() {
   const userId = getUserId();
   return (
-    <header className="flex top-0 max-w-[1920px] h-20 px-4 justify-between bg-gradient-to-b from-navbarStart to-secondary">
-      <Logo />
-      <section className="flex md:space-x-4 space-x-3 w-auto items-center">
-        <SignedOut>
-          <Link
-            className={cn("font-semibold text-xl", source_sans_3.className)}
-            href={"/sign-in"}
-          >
-            Logga in
-          </Link>
-        </SignedOut>
-        <SignedIn>
-          {(checkRole("admin") || checkRole("moderator")) && (
-            <Link href="/admin">
-              <LockKeyhole strokeWidth={1} className="md:hidden block" />
+    <header className="flex top-0 h-20 bg-gradient-to-b from-navbarStart to-secondary">
+      <div className="flex justify-between items-center w-full max-w-[1920px] mx-auto px-4">
+        <Logo />
+        <section className="flex md:space-x-4 space-x-3 w-auto items-center">
+          <SignedOut>
+            <Link
+              className={cn("font-semibold text-xl", source_sans_3.className)}
+              href={"/sign-in"}
+            >
+              Logga in
+            </Link>
+          </SignedOut>
+          <SignedIn>
+            {(checkRole("admin") || checkRole("moderator")) && (
+              <Link href="/admin">
+                <LockKeyhole strokeWidth={1} className="md:hidden block" />
+                <p
+                  className={cn(
+                    "text-xl font-medium md:block hidden",
+                    source_sans_3.className
+                  )}
+                >
+                  Admin Panel
+                </p>
+              </Link>
+            )}
+            <Link href="/create-post">
+              <PlusSquare strokeWidth={1} className="md:hidden block" />
               <p
                 className={cn(
                   "text-xl font-medium md:block hidden",
                   source_sans_3.className
                 )}
               >
-                Admin Panel
+                Skapa inlägg
               </p>
             </Link>
-          )}
-          <Link href="/create-post">
-            <PlusSquare strokeWidth={1} className="md:hidden block" />
-            <p
-              className={cn(
-                "text-xl font-medium md:block hidden",
-                source_sans_3.className
-              )}
-            >
-              Skapa inlägg
-            </p>
-          </Link>
-          <Link href={`/profile/${userId}`}>
-            <BookUser strokeWidth={1} className="md:hidden block" />
-            <p
-              className={cn(
-                "text-xl font-medium md:block hidden",
-                source_sans_3.className
-              )}
-            >
-              Min sida
-            </p>
-          </Link>
-          <UserButton
-            afterSignOutUrl="/"
-            appearance={{
-              elements: {
-                avatarBox: {
-                  height: 30,
-                  width: 30,
+            <Link href={`/profile/${userId}`}>
+              <BookUser strokeWidth={1} className="md:hidden block" />
+              <p
+                className={cn(
+                  "text-xl font-medium md:block hidden",
+                  source_sans_3.className
+                )}
+              >
+                Min sida
+              </p>
+            </Link>
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                elements: {
+                  avatarBox: {
+                    height: 30,
+                    width: 30,
+                  },
                 },
-              },
-            }}
-          />
-        </SignedIn>
-      </section>
+              }}
+            />
+          </SignedIn>
+        </section>
+      </div>
     </header>
   );
 }
