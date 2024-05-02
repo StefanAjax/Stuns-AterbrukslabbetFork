bda5b66f2f7d4aae77d96daebb343a219ab88c01
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2024-05-02 15:45:33 +0200

    Fix search bar (#76)
    * Fix search bar
    
    ---------
    
    Co-authored-by: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>
    Co-authored-by: lukasgurra <142992155+lukasgurra@users.noreply.github.com>

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 import handleSearchParamsChange from "@/utils/handle-search-params-change";
 
@@ -16,10 +17,22 @@ export default function SearchBar({
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
+  const [showHint, setShowHint] = useState(false);
+  const searchParamValue = searchParams.get("search");
 
   function handleSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const search = e.target.value;
-    handleSearchParamsChange("search", search, pathname, searchParams, replace);
+    const searchInput = e.target.value;
+    let query;
+    // Clerk's "getUserList" function only accepts queries larger than two characters, hence
+    // why the query is undefined up until the input has reached a length of atleast 3 characters.
+    if (searchInput && searchInput.length < 3) {
+      query = undefined;
+      setShowHint(true);
+    } else {
+      query = searchInput;
+      setShowHint(false);
+    }
+    handleSearchParamsChange("search", query, pathname, searchParams, replace);
   }
 
   return (
@@ -33,18 +46,20 @@ export default function SearchBar({
         <label htmlFor="search" className="md:text-lg text-sm font-medium">
           {labelText}
         </label>
-        {searchParams.get("search") && (
-          <p className="md:text-base text-xs">
-            {`${itemsFoundCount} resultat`}
-          </p>
-        )}
+        {searchParamValue
+          ? searchParamValue.length > 2 && (
+              <p className="md:text-base text-xs">
+                {`${itemsFoundCount} resultat`}
+              </p>
+            )
+          : showHint && <p className="md:text-base text-xs">Minst 3 tecken</p>}
       </div>
       <input
         id="search"
         className="rounded-md bg-primary md:h-12 h-9 md:px-3 px-2 md:text-lg text-sm"
         placeholder="Sök..."
         onChange={handleSearchChange}
-        defaultValue={searchParams.get("search")?.toString()}
+        defaultValue={searchParamValue?.toString()}
         autoComplete="on"
       />
     </form>
