866b64aa1421add84cae0bb73782c2cff3e1ba8c
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-05-02 15:15:18 +0200

    Fix error in mail automation script example (#75)

--- expiring-post-mail-automation/expiring-post-mail-automation.sh
+++ expiring-post-mail-automation/expiring-post-mail-automation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RESULT=$(curl --location --request POST "http://localhost:3000/api/send-mail-to-expiring-posts" \ --header "secret: YOUR SECRET KEY HERE")
+RESULT=$(curl --location --request POST "http://localhost:3000/api/send-mail-to-expiring-posts" \
+--header "secret: YOUR SECRET KEY HERE")
 
 CURRENT_DATE=$(date)
 
