63122aa1f07177c40fce893cbabed1ad21f92249
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-02-27 10:45:09 +0100

    Fixed issue with middleware infinite redirect loop
    Co-Authored-By: David Cavalli-Björkman <119424270+DavidCavalliB@users.noreply.github.com>
    Co-Authored-By: EskilNTI <178795262+EskilNTI@users.noreply.github.com>
    Co-Authored-By: Tim Kelso <119423866+NTI-Tim@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,3 @@
-// import { NextResponse } from "next/server";
-
-// import { authMiddleware } from "@clerk/nextjs";
-
-// export default authMiddleware({
-//   // Routes that can be accessed while signed out
-//   publicRoutes: [
-//     "/",
-//     "/terms-of-service",
-//     "/faq",
-//     "/about",
-//     "/api/webhooks(.*)",
-//     "/api/send-mail-to-expiring-posts(.*)",
-//     "/delete(.*)",
-//     "/extend(.*)",
-//   ],
-
-//   afterAuth(auth, req) {
-//     if (!auth.userId && !auth.isPublicRoute) {
-//       const path = new URL(req.url).pathname;
-//       return NextResponse.redirect(
-//         new URL("/sign-in?redirect_url=" + path, req.url),
-//       );
-//     }
-//   },
-// });
-
-// export const config = {
-//   // Protects all routes, including api/trpc.
-//   // See https://clerk.com/docs/references/nextjs/auth-middleware
-//   // for more information about configuring your Middleware
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
@@ -43,12 +9,14 @@ const isPublicRoute = createRouteMatcher([
   "/api/send-mail-to-expiring-posts(.*)",
   "/delete(.*)",
   "/extend(.*)",
+  "/sign-in",
+  "/sign-up",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
   if (!userId && !isPublicRoute(req)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
+    return redirectToSignIn({ returnBackUrl: "/sign-in" });
   }
 });
 
