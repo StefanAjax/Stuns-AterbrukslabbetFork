5f9fb9448cda9a84e67418c81210872acf07ef7a
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2025-02-28 09:40:03 +0100

    Change config files for increased consistency

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 200,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  endOfLine: "lf",
+};
