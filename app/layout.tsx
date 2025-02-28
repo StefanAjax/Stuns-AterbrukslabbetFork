4a34893dfb09d9c18d2173ec29849d96ac31200c
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-02-28 10:08:35 +0100

    Added more metadata for SEO reasons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,18 +7,63 @@ import { svSE } from "@clerk/localizations";
 
 import "./globals.css";
 
+const description: string =
+  "Återbrukslabbet ger avställd labbutrustning nytt liv i skolan. Modern utrustning ökar undervisningens relevans, avlastar skolbudgetar, sparar planetens resurser och gör lärandet roligare. Företag har möjlighet att donera utrustning, inventarier och förbrukningsmaterial som de själva inte längre har nytta av. Lärare har möjlighet att efterfråga sådant som de behöver för att bedriva inspirerande och kvalitativ undervisning.";
+
 export const metadata: Metadata = {
-  title: "Återbrukslabbet",
+  alternates: {
+    canonical: "https://aterbrukslabbet.nu/",
+  },
+  authors: [
+    {
+      name: "Simon Clavensjö",
+      url: "https://simon.clavensjo.se/",
+    },
+    {
+      name: "Lukas Gustafsson",
+      url: "https://lukasgurra.github.io/",
+    },
+    {
+      name: "Mohamad Hamdan",
+      url: "https://mohamadhamdan13.github.io/",
+    },
+    {
+      name: "Ambjörn Hogmark",
+      url: "https://ambjorn-hogmark.github.io/",
+    },
+    {
+      name: "Axel Thornberg",
+      url: "https://axel.thornberg.se/",
+    },
+  ],
+  creator: "STUNS Life science",
+  description: description,
+  generator: "Next.js",
   icons: {
     icon: [
       {
         url: "/favicons/favicon.ico",
         sizes: "any",
         type: "image/x-icon",
-        href: "/favicons/favicon.ico",
       },
     ],
   },
+  keywords: ["återbrukslabbet", "aterbrukslabbet", "återbrukslabb", "aterbrukslabb"],
+  openGraph: {
+    type: "website",
+    title: "Återbrukslabbet",
+    url: "https://aterbrukslabbet.nu/",
+    images: [
+      {
+        url: "https://aterbrukslabbet.nu/favicons/favicon.ico",
+      },
+    ],
+    description: description,
+    determiner: "",
+    locale: "sv_SE",
+  },
+  publisher: "STUNS Life science",
+  title: "Återbrukslabbet",
 };
 
 export default function RootLayout({
