7095cbcfdbea04a156f3615ba995f8e542737559
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2025-03-05 09:36:29 +0100

    New color scheme for a cohesive brand

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -12,66 +11,125 @@ const config = {
         "2xl": "1400px",
       },
     },
+    colors: {
+      black: "hsl(var(--black))",
+      white: "hsl(var(--white))",
+      neutral: {
+        "50": "hsl(var(--neutral-50))",
+        "100": "hsl(var(--neutral-100))",
+        "200": "hsl(var(--neutral-200))",
+        "300": "hsl(var(--neutral-300))",
+        "400": "hsl(var(--neutral-400))",
+        "500": "hsl(var(--neutral-500))",
+        "600": "hsl(var(--neutral-600))",
+        "700": "hsl(var(--neutral-700))",
+        "800": "hsl(var(--neutral-800))",
+        "900": "hsl(var(--neutral-900))",
+        "950": "hsl(var(--neutral-950))",
+      },
+      green: {
+        "50": "hsl(var(--green-50))",
+        "100": "hsl(var(--green-100))",
+        "200": "hsl(var(--green-200))",
+        "300": "hsl(var(--green-300))",
+        "400": "hsl(var(--green-400))",
+        "500": "hsl(var(--green-500))",
+        "600": "hsl(var(--green-600))",
+        "700": "hsl(var(--green-700))",
+        "800": "hsl(var(--green-800))",
+        "900": "hsl(var(--green-900))",
+        "950": "hsl(var(--green-950))",
+      },
+      blue: {
+        "50": "hsl(var(--blue-50))",
+        "100": "hsl(var(--blue-100))",
+        "200": "hsl(var(--blue-200))",
+        "300": "hsl(var(--blue-300))",
+        "400": "hsl(var(--blue-400))",
+        "500": "hsl(var(--blue-500))",
+        "600": "hsl(var(--blue-600))",
+        "700": "hsl(var(--blue-700))",
+        "800": "hsl(var(--blue-800))",
+        "900": "hsl(var(--blue-900))",
+        "950": "hsl(var(--blue-950))",
+      },
+      orange: {
+        "50": "hsl(var(--orange-50))",
+        "100": "hsl(var(--orange-100))",
+        "200": "hsl(var(--orange-200))",
+        "300": "hsl(var(--orange-300))",
+        "400": "hsl(var(--orange-400))",
+        "500": "hsl(var(--orange-500))",
+        "600": "hsl(var(--orange-600))",
+        "700": "hsl(var(--orange-700))",
+        "800": "hsl(var(--orange-800))",
+        "900": "hsl(var(--orange-900))",
+        "950": "hsl(var(--orange-950))",
+      },
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
 
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "#f1f5f9",
-        foreground: "hsl(var(--foreground))",
-        primary: "#d9d9d9",
-        secondary: "#ffffff",
-        navbarStart: "#ECECEC",
-        offerColor: "#10b981",
-        requestColor: "#2563eb",
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
-        },
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      secondary: {
+        DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+      destructive: {
+        DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+      muted: {
+        DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
       },
+      accent: {
+        DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
+      },
+      popover: {
+        DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
+      },
+      card: {
+        DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
+      },
+
+      offer: "hsl(var(--offer))",
+      request: "hsl(var(--request))",
+
+      chart: {
+        "1": "hsl(var(--chart-1))",
+        "2": "hsl(var(--chart-2))",
+        "3": "hsl(var(--chart-3))",
+        "4": "hsl(var(--chart-4))",
+        "5": "hsl(var(--chart-5))",
+      },
+    },
+    borderRadius: {
+      lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      sm: "calc(var(--radius) - 4px)",
+    },
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
     },
   },
   plugins: [require("tailwindcss-animate")],
