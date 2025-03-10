fa3a21f3e94bc187040ea9c1052d2113e4873421
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2025-03-10 11:30:29 +0100

    Refactor Intro component to adjust for recent SVG 'layered waves' changes

--- app/(main-page)/_components/intro.tsx
+++ app/(main-page)/_components/intro.tsx
@@ -7,17 +7,17 @@ import layeredWaves from "@/public/images/layered-waves/emerald-light.svg";
 export default function Intro() {
   return (
     <>
-      <div className="flex flex-col items-center bg-emerald-500 py-10 text-white">
+      <div className="flex flex-col bg-emerald-500 py-10 text-center text-white">
         <div className={cn("text-3xl font-semibold md:text-5xl", source_sans_3.className)}>Välkommen till</div>
         <div className={cn("text-4xl font-medium md:text-6xl", prompt.className)}>Återbrukslabbet</div>
-        <div className={cn("text-balance py-3 px-20 text-center md:text-3xl mt-2 md:mt-5", source_sans_3.className)}>
+        <div className={cn("mt-2 text-balance px-20 py-3 md:mt-5 md:text-3xl", source_sans_3.className)}>
           <p>
             Återbrukslabbet ger avställd labbutrustning nytt liv i skolan. Modern utrustning ökar undervisningens relevans, avlastar skolbudgetar, sparar planetens resurser och gör lärandet roligare.
           </p>
           <p>Företag kan donera, lärare kan efterfråga. Välkommen till Återbrukslabbet!</p>
         </div>
       </div>
-      <Image src={layeredWaves} alt="" draggable="false" className="w-full" />
+      <Image src={layeredWaves} alt="" draggable="false" className="max-h-[30rem] min-h-56" />
     </>
   );
 }
