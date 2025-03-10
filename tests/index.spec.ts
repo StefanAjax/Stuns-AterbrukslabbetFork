081716176c94179fc143bdd16e944ecc9918950f
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-10 15:58:46 +0100

    Wrote simple content tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,8 +2,32 @@ import { test, expect } from "@playwright/test";
 
 test.describe.configure({ mode: "parallel" });
 
-test("has title", async ({ page }) => {
+test("Title", async ({ page }) => {
   await page.goto("/");
 
-  expect(await page.locator("text=Återbrukslabbet").filter({ visible: true }).count()).toBeGreaterThanOrEqual(1);
+  await expect(page.getByText("Återbrukslabbet").filter({ visible: true }).first()).toBeVisible();
+});
+
+test("No posts", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByText("Inga annonser hittades").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Sök bland 0 annonser").filter({ visible: true }).first()).toBeVisible();
+});
+
+test("Categories", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByText("Alla").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Erbjuds").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Efterfrågas").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Förbrukningsvara").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Instrument/Maskin").filter({ visible: true }).first()).toBeVisible();
+
+  await expect(page.getByText("Inventarie").filter({ visible: true }).first()).toBeVisible();
 });
