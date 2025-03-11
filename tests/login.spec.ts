27e6b59a43645cf89c2579fe38fc0d3d48f6f643
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-11 11:42:22 +0100

    Added login test functionality
    Co-Authored-By: EskilNTI <178795262+EskilNTI@users.noreply.github.com>

---
+++ tests/login.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+test("Login", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByText("Logga in").filter({ visible: true }).first()).toBeVisible();
+
+  await page.getByText("Logga in").first().click();
+
+  await expect(page).toHaveURL("http://localhost:3000/sign-in");
+
+  await expect(page.locator("#identifier-field")).toBeVisible();
+
+  await page
+    .locator("#identifier-field")
+    .first()
+    .fill(process.env.TESTING_EMAIL || "");
+
+  await expect(page.locator("#password-field")).toBeVisible();
+
+  await page
+    .locator("#password-field")
+    .first()
+    .fill(process.env.TESTING_PASSWORD || "");
+
+  await page.getByRole("button").filter({ hasText: "Fortsätt" }).first().click();
+
+  await expect(page).toHaveURL("http://localhost:3000/");
+
+  await expect(page.getByText("Mina annonser").filter({ visible: true }).first()).toBeVisible();
+});
