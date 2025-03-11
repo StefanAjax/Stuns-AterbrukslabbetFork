5a68aad77a0ef529c774a7e083ee6908fb1c8c6d
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-11 11:17:41 +0100

    Removed parallel mode from tests
    Co-Authored-By: EskilNTI <178795262+EskilNTI@users.noreply.github.com>

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.configure({ mode: "parallel" });
-
 test("Title", async ({ page }) => {
   await page.goto("/");
 
--- tests/static.spec.ts
+++ tests/static.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test.describe.configure({ mode: "parallel" });
-
 test("FAQ", async ({ page }) => {
   await page.goto("/");
 
