44865b35dcf0814f3a8c242a16378479524e5d3e
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-12 09:32:30 +0100

    Increased the wrong timeout. Should be good now

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
+  timeout: process.env.CI ? 300000 : 60000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
