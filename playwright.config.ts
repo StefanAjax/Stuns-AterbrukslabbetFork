12ff60f73e457131541c3d087fcbc69caed07c36
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-12 15:59:57 +0100

    Updated comments on playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   retries: 0,
   /* Timeout for each test */
   timeout: 60000,
-  /* Opt out of parallel tests on CI. */
+  /* Only run one test at a time */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
@@ -33,17 +33,18 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-
+    /* Viewport size for the browser. */
     viewport: { width: 2560, height: 1440 },
-
+    /* Record videos of the tests. */
     video: {
       mode: "on",
+      /* Resolution of the videos */
       size: { width: 2560, height: 1440 },
     },
     launchOptions: {
+      /* Add a delay of 1 second between each action. */
       slowMo: 1000,
     },
   },
@@ -66,26 +67,6 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
