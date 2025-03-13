df96a70c6b90523d41669b5cb76739d5573b6444
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-13 09:04:07 +0100

    Removed webkit from testing.

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,15 +57,9 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
     },
   ],
 
