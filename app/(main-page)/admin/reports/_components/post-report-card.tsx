e65a13437959ba0dfbf46e00938d332448c5aeea
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-03-26 15:59:40 +0100

    Continued styling the reported posts page

--- app/(main-page)/admin/reports/_components/post-report-card.tsx
+++ app/(main-page)/admin/reports/_components/post-report-card.tsx
@@ -1,31 +1,37 @@
-import { inter } from "@/app/fonts";
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs/server";
+
+import type { Post } from "@prisma/client";
 
 interface PostReportCardProps {
-   report: {
-      id: number;
-      reason: string;
-      postLink: string;
-      reporterLink: string;
-      createdAt: Date;
-   };
+  report: {
+    id: number;
+    reason: string;
+    postLink: string;
+    reporterLink: string;
+    createdAt: Date;
+    post: Post;
+  };
 }
 
-export default async function PostReportCard({ report } : PostReportCardProps) {
+export default async function PostReportCard({ report }: PostReportCardProps) {
+  const user = await currentUser();
   return (
     <div key={report.id} className="flex w-full justify-between gap-x-10 rounded-md bg-secondary p-3 md:gap-x-20">
       <p>{report.reason}</p>
       <span>
+        <p>Annons som blivit rapporterad:</p>
         <p>
-          <Link href={report.postLink}>Annons som blivit rapporterad</Link>
+          <Link href={report.postLink}>{report.post.title}</Link>
         </p>
       </span>
       <span>
+        <p>Användare som rapporterade annonsen:</p>
         <p>
-          <Link href={report.reporterLink}>Användare som rapporterade annonsen</Link>
+          <Link href={report.reporterLink}>{user?.fullName}</Link>
         </p>
       </span>
-      <p>Reported at {report.createdAt.toDateString()}</p>
+      <p>Blev rapporterad: {report.createdAt.toLocaleDateString()}</p>
     </div>
   );
 }
--- app/(main-page)/admin/reports/page.tsx
+++ app/(main-page)/admin/reports/page.tsx
@@ -4,7 +4,11 @@ import { db } from "@/lib/db";
 import PostReportCard from "./_components/post-report-card";
 
 export default async function AdminReportsPage() {
-  const reports = await db.reportedPosts.findMany();
+  const reports = await db.reportedPosts.findMany({
+    include: {
+      post: true,
+    },
+  });
   return (
     <>
       <div className="mx-auto max-w-screen-md p-3 pt-10">
