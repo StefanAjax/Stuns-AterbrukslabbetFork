5db48fea96867b7cf9f0bb06e256df02e9ba24c5
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-10 16:01:57 +0200

    Might have fixed timeout related issue for toast popup for report status

--- tests/report-post.spec.ts
+++ tests/report-post.spec.ts
@@ -57,7 +57,7 @@ test("Report Post", async ({ page, browserName }) => {
 
   await screenshot(page, browserName);
 
-  await expect(page.getByText("Annonsen har blivit rapporterad").filter({ visible: true }).first()).toBeVisible();
+  await expect(page.getByText("Annonsen har blivit rapporterad").filter({ visible: true }).first()).toBeVisible({ timeout: 30000 });
 
   await logout(page);
 
