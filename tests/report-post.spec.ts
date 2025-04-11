205205876d8ad349b14a3fa38621b4336e86dbaf
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-11 10:47:15 +0200

    Chore: Formatting

--- tests/report-post.spec.ts
+++ tests/report-post.spec.ts
@@ -61,7 +61,7 @@ test("Report Post", async ({ page, browserName }) => {
 
   // I know this is againt best practices, but playwright is not able to detect the toast nor the Next.js response for when posts are reported
   await page.waitForTimeout(20000);
-  
+
   await logout(page);
 
   await expect(page.getByText("Logga in").filter({ visible: true }).first()).toBeVisible();
@@ -71,7 +71,6 @@ test("Report Post", async ({ page, browserName }) => {
   await page.getByText("Adminpanel").first().click();
 
   await screenshot(page, browserName);
-
 
   await page.getByText("Rapporter").first().click();
 
