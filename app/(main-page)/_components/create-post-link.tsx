b5ff0b2d82f65b6eed42c15e56d244f28f0a669d
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2025-04-11 11:40:35 +0200

    Format project

--- app/(main-page)/_components/create-post-link.tsx
+++ app/(main-page)/_components/create-post-link.tsx
@@ -3,7 +3,7 @@ import { Plus } from "lucide-react";
 
 export default function CreatePostLink() {
   return (
-    <Link className="bg-sky-600 flex items-center gap-x-1 rounded-md px-2 py-[6px] text-white hover:opacity-85 md:gap-x-2 md:px-4 md:py-2" href="/create-post">
+    <Link className="flex items-center gap-x-1 rounded-md bg-sky-600 px-2 py-[6px] text-white hover:opacity-85 md:gap-x-2 md:px-4 md:py-2" href="/create-post">
       <Plus className="h-4 w-4 md:h-6 md:w-6" />
       <p className="text-xs md:text-lg">Skapa annons</p>
     </Link>
