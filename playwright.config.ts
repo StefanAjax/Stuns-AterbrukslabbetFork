089b46b39b6d6c199334517f6b967fdf4c37c3c3
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-14 10:27:19 +0200

    Testing to see if slowMo is required or not

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       size: { width: 2560, height: 1440 },
     },
     launchOptions: {
-      /* Add a delay of 1 second between each action. */
-      slowMo: 1000,
+      /* Add a delay of 0 second between each action. */
+      slowMo: 0,
     },
   },
 
