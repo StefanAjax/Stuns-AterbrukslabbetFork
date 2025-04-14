06c853ce5106d524f02addcc741bbd1466b4c6d7
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-14 10:39:29 +0200

    .25 was acceptable, decreasing to a tenth of a second

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       size: { width: 2560, height: 1440 },
     },
     launchOptions: {
-      /* Add a delay of .25 seconds between each action. */
-      slowMo: 250,
+      /* Add a delay of .1 seconds between each action. */
+      slowMo: 100,
     },
   },
 
