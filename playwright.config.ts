2e834d3262feba94c645b4e9ca1cd1619377ed7e
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-14 14:39:55 +0200

    .15 works quite well but is periodically a bit flaky. Increasing to .2

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       size: { width: 2560, height: 1440 },
     },
     launchOptions: {
-      /* Add a delay of .15 seconds between each action. */
-      slowMo: 150,
+      /* Add a delay of .2 seconds between each action. */
+      slowMo: 200,
     },
   },
 
