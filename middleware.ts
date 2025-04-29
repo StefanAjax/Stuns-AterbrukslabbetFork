4f2ac4450560ac4e94d4013457254411a3be4b3a
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-04-29 12:42:01 +0200

    Fixed image rendering bug

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ const isPublicRoute = createRouteMatcher([
   /^\/sitemap(-\d+)?\.xml$/,
   "/robots.txt",
   "/favicons/favicon.ico",
+  "/uploads(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
