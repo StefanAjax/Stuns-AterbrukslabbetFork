d33124b2c0c6a166610bdefc28a80857c23a7bb1
NTI-Tim <tim.kelso@elev.ga.ntig.se>
2025-04-29 14:36:26 +0200

    Add DeveloperProfile component for DRY purposes

--- app/(main-page)/about/page.tsx
+++ app/(main-page)/about/page.tsx
@@ -1,3 +1,5 @@
+import DeveloperProfile from "./_components/developer-profile";
+
 export default function AboutPage() {
   return (
     <div className="mx-auto mt-12 flex w-[85ch] max-w-full flex-col bg-secondary px-6 py-8 md:mt-16 md:rounded-md">
@@ -15,102 +17,46 @@ export default function AboutPage() {
         Den första etappen genomfördes 2024. Under våren 2025 utvecklar ett nytt team nya funktioner, samt genomför underhåll av de befintliga. Samtidigt testas Återbrukslabbet av skolor och företag i
         Uppsala med omnejd.
       </p>
-      <div className="flex flex-col gap-y-3">
+      <div className="mb-8 flex flex-col gap-y-6">
         <h2 className="text-xl font-semibold">Utvecklare våren 2024:</h2>
-        <div>
-          <span className="font-semibold">Simon Clavensjö</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/simon-clavensj%C3%B6-a6059021b/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://github.com/SimonClavensjo">
-            Github
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://simon.clavensjo.se/">
-            portfolio
-          </a>
-        </div>
-        <div>
-          <span className="font-semibold">Lukas Gustafsson</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/lukas-gustafsson-b4296b293/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://github.com/lukasgurra">
-            Github
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://lukasgurra.github.io/">
-            portfolio
-          </a>
-          <p>Planerar att utbilda sig till danslärare eller teknik- och mattelärare. Intresserad av frontend-utveckling och design av spel och hemsidor.</p>
-        </div>
-        <div>
-          <span className="font-semibold">Mohamad Hamdan</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/mohamad-hamdan-874670297/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://mohamadhamdan13.github.io/">
-            portfolio
-          </a>
-          <p>Jobbar på Nordic Wellness och inleder studier inom systemvetenskap hösten 2025.</p>
-        </div>
-        <div>
-          <span className="font-semibold">Ambjörn Hogmark</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/ambj%C3%B6rn-hogmark-a86801293/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://ambjorn-hogmark.github.io/">
-            portfolio
-          </a>
-          <p>Studerar till civilingenjör på KTH.</p>
-        </div>
+        <DeveloperProfile
+          name="Simon Clavensjö"
+          linkedInUrl="https://www.linkedin.com/in/simon-clavensj%C3%B6-a6059021b/"
+          githubUrl="https://github.com/SimonClavensjo"
+          portfolioUrl="https://simon.clavensjo.se/"
+        />
+        <DeveloperProfile
+          name="Lukas Gustafsson"
+          linkedInUrl="https://www.linkedin.com/in/lukas-gustafsson-b4296b293/"
+          githubUrl="https://github.com/lukasgurra"
+          portfolioUrl="https://lukasgurra.github.io/"
+          bio="Planerar att utbilda sig till danslärare eller teknik- och mattelärare. Intresserad av frontend-utveckling och design av spel och hemsidor."
+        />
+        <DeveloperProfile
+          name="Mohamad Hamdan"
+          linkedInUrl="https://www.linkedin.com/in/mohamad-hamdan-874670297/"
+          portfolioUrl="https://mohamadhamdan13.github.io/"
+          bio="Jobbar på Nordic Wellness och inleder studier inom systemvetenskap hösten 2025."
+        />
+        <DeveloperProfile
+          name="Ambjörn Hogmark"
+          linkedInUrl="https://www.linkedin.com/in/ambj%C3%B6rn-hogmark-a86801293/"
+          portfolioUrl="https://ambjorn-hogmark.github.io/"
+          bio="Studerar till civilingenjör på KTH."
+        />
       </div>
 
-      <div className="mt-8 flex flex-col gap-y-3">
+      <div className="mb-8 flex flex-col gap-y-6">
         <h2 className="text-xl font-semibold">Utvecklare våren 2025:</h2>
-        <div>
-          <span className="font-semibold">David Cavalli-Björkman</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/david-cavalli-bj%C3%B6rkman/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://davidcavallib.github.io/">
-            portfolio
-          </a>
-        </div>
-        <div>
-          <span className="font-semibold">Tim Kelso</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://linkedin.com/in/kelsotim/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://github.com/TimKelso">
-            Github
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://timkelso.github.io/">
-            portfolio
-          </a>
-        </div>
-        <div>
-          <span className="font-semibold">Axel Thornberg</span>,{" "}
-          <a className="text-blue-600 hover:underline" href="https://www.linkedin.com/in/axel-thornberg-618a41332/">
-            LinkedIn
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://github.com/axelNTI">
-            GitHub
-          </a>
-          ,{" "}
-          <a className="text-blue-600 hover:underline" href="https://axel.thornberg.se/">
-            portfolio
-          </a>
-          <p>Kommer att studera datavetenskap på Uppsala Universitet från hösten 2025.</p>
-        </div>
+        <DeveloperProfile name="David Cavalli-Björkman" linkedInUrl="https://www.linkedin.com/in/david-cavalli-bj%C3%B6rkman/" portfolioUrl="https://davidcavallib.github.io/" />
+        <DeveloperProfile name="Tim Kelso" linkedInUrl="https://linkedin.com/in/kelsotim/" githubUrl="https://github.com/TimKelso" portfolioUrl="https://timkelso.github.io/" />
+        <DeveloperProfile
+          name="Axel Thornberg"
+          linkedInUrl="https://www.linkedin.com/in/axel-thornberg-618a41332/"
+          githubUrl="https://github.com/axelNTI"
+          portfolioUrl="https://axel.thornberg.se/"
+          bio="Kommer att studera datavetenskap på Uppsala Universitet från hösten 2025."
+        />
       </div>
       <p className="mt-6">
         Om du har frågor, eller vill ge feedback på verktyget, vänligen skriv en rad till{" "}
