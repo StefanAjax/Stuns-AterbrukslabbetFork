0f48e5521945610707466365372f118dac2c66a5
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-07 14:09:54 +0200

    Ensured all variables that can change under a test are reset before each test

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,10 +1,19 @@
 import { expect } from "@playwright/test";
 import type { Page } from "@playwright/test";
 import { db } from "@/lib/db";
+import path from "node:path";
+import fs from "node:fs";
 import { clerk, clerkSetup, setupClerkTestingToken } from "@clerk/testing/playwright";
 
 export const setup = async (page: Page) => {
   await db.post.deleteMany({});
+
+  await db.resources.deleteMany({});
+
+  const clientDir = path.join(process.cwd(), "client");
+  if (fs.existsSync(clientDir)) {
+    fs.rmSync(clientDir, { recursive: true, force: true });
+  }
 
   await page.goto("/");
 };
