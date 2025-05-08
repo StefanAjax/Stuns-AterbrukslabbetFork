95a804b8d4ccce9429594f51d62c136146a03686
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-08 09:17:07 +0200

    Fixed issue with admin dropdown changing value when choosing a page

--- components/admin-dropdown.tsx
+++ components/admin-dropdown.tsx
@@ -35,7 +35,7 @@ export default function AdminDropdown({ className }: AdminDropdownProps) {
         <SelectTrigger
           className={cn("flex select-none items-center gap-x-1 border-none bg-transparent text-lg font-medium shadow-none transition-colors hover:text-accent focus:ring-0", source_sans_3.className)}
         >
-          <SelectValue placeholder="Adminpanel" />
+          <SelectValue placeholder="Adminpanel">Adminpanel</SelectValue>
         </SelectTrigger>
         <SelectContent>
           {adminMenuItems.map((item) => (
